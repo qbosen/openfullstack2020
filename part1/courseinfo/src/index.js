40c1b1d17e707e233eff3aa8f8d2a2586e43550a
qiubaisen <abosen@qq.com>
2020-10-28 18:11:08 +0800

    exercises 1.3-1.5

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -1,8 +1,8 @@
 import ReactDOM from 'react-dom'
 
-const Header = (props) => (<h1>{props.course}</h1>)
-const Part = (props) => (<p>{props.part} {props.exercises}</p>)
-const Content = (props) => props.contents.map(Part)
+const Header = (props) => (<h1>{props.course.name}</h1>)
+const Part = (props) => (<p>{props.name} {props.exercises}</p>)
+const Content = (props) => props.parts.map(Part)
 const Total = (props) => {
     let sum = 0
     props.exercises.forEach(it => sum += it)
@@ -11,35 +11,29 @@ const Total = (props) => {
 
 
 const App = () => {
-    const course = 'Half Stack application development'
-    const part1 = 'Fundamentals of React'
-    const exercises1 = 10
-    const part2 = 'Using props to pass data'
-    const exercises2 = 7
-    const part3 = 'State of a component'
-    const exercises3 = 14
-
-    const contents = [
-        {
-            part: part1,
-            exercises: exercises1
-        },
-        {
-            part: part2,
-            exercises: exercises2
-        },
-        {
-            part: part3,
-            exercises: exercises3
-        }
-    ]
-    const exercises = [exercises1, exercises2, exercises3]
+    const course = {
+        name: 'Half Stack application development',
+        parts: [
+            {
+                name: 'Fundamentals of React',
+                exercises: 10
+            },
+            {
+                name: 'Using props to pass data',
+                exercises: 7
+            },
+            {
+                name: 'State of a component',
+                exercises: 14
+            }
+        ]
+    }
 
     return (
         <div>
             <Header course={course}/>
-            <Content contents={contents}/>
-            <Total exercises={exercises}/>
+            <Content parts={course.parts}/>
+            <Total exercises={course.parts.map(it => it.exercises)}/>
         </div>
     )
 }
