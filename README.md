123516fba6845b94bec66d48bdd10050782bda09
qiubaisen <abosen@qq.com>
2020-10-30 18:07:30 +0800

    2.11

--- README.md
+++ README.md
@@ -5,3 +5,14 @@ sudo npm install -g n
 sudo n stable
 sudo npm install npm@latest -g
 ```
+
+## install json server
+```
+sudo npm install -g json-server
+npx json-server --port 3001 --watch db.json
+
+// develop dependency
+npm install json-server --save-dev
+// add to package.json
+"server": "json-server -p3001 --watch db.json"
+```
--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,19 +1,15 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
 import Persons from "./components/Person.js";
+import axios from 'axios'
 
 const App = () => {
-    const [persons, setPersons] = useState([
-        {name: 'Arto Hellas', number: '040-123456'},
-        {name: 'Ada Lovelace', number: '39-44-5323523'},
-        {name: 'Dan Abramov', number: '12-43-234345'},
-        {name: 'Mary Poppendieck', number: '39-23-6423122'}
-    ])
+    const [persons, setPersons] = useState([])
     const [newName, setNewName] = useState('')
     const [newNumber, setNewNumber] = useState('')
     const [filter, setFilter] = useState('')
-    const [shownPersons, setShownPersons] = useState(persons)
+    const shownPersons = [...persons.filter(it => it.name.toLowerCase().startsWith(filter))]
 
     const onNameChange = (event) => setNewName(event.target.value)
     const onNumberChange = (event) => setNewNumber(event.target.value)
@@ -27,8 +23,13 @@ const App = () => {
     }
     const onFilterChange = (event) => {
         setFilter(event.target.value)
-        setShownPersons([...persons.filter(it => it.name.toLowerCase().startsWith(event.target.value))])
     }
+    useEffect(() => {
+            axios
+                .get('http://localhost:3001/persons')
+                .then(res => setPersons(res.data))
+        }
+        , [])
 
     return (
         <div>
