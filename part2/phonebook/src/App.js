997ca4fe399be53f2a9ec72a536af10b1a2f51dd
qiubaisen <abosen@qq.com>
2020-10-30 15:21:29 +0800

    2.9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -3,10 +3,15 @@ import Person from './components/Person.js'
 
 const App = () => {
     const [persons, setPersons] = useState([
-        {name: 'Arto Hellas', number: '040-1234567'}
+        {name: 'Arto Hellas', number: '040-123456'},
+        {name: 'Ada Lovelace', number: '39-44-5323523'},
+        {name: 'Dan Abramov', number: '12-43-234345'},
+        {name: 'Mary Poppendieck', number: '39-23-6423122'}
     ])
     const [newName, setNewName] = useState('')
     const [newNumber, setNewNumber] = useState('')
+    const [filter, setFilter] = useState('')
+    const [shownPersons, setShownPersons] = useState(persons)
 
     const onNameChange = (event) => setNewName(event.target.value)
     const onNumberChange = (event) => setNewNumber(event.target.value)
@@ -18,11 +23,19 @@ const App = () => {
             setPersons([...persons, {name: newName, number: newNumber}])
         }
     }
-
+    const refresh = (event) => {
+        setFilter(event.target.value)
+        setShownPersons([...persons.filter(it => it.name.toLowerCase().startsWith(event.target.value))])
+        console.log(shownPersons)
+    }
 
     return (
         <div>
             <h2>Phonebook</h2>
+            <div>
+                filter shown with <input onChange={refresh} value={filter}/>
+            </div>
+            <h2>add a new</h2>
             <form onSubmit={onSubmit}>
                 <div>
                     name: <input onChange={onNameChange} value={newName}/>
@@ -35,8 +48,7 @@ const App = () => {
                 </div>
             </form>
             <h2>Numbers</h2>
-
-            {persons.map(it => <Person key={it.name} person={it}/>)}
+            {shownPersons.map(it => <Person key={it.name} person={it}/>)}
         </div>
     )
 }
